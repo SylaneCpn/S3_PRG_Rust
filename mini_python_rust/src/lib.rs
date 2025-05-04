6132b41d69b35b7772b4821d5644765e7db9af44
SylaneCpn <sylane.campan@gmail.com>
2025-05-04 23:23:41 +0200

    fmt

--- mini_python_rust/src/lib.rs
+++ mini_python_rust/src/lib.rs
@@ -5,28 +5,24 @@ fn say_hello() {
     println!("Hello from Rust!");
 }
 
-
 #[no_mangle]
-fn compute(first : c_double , second : c_double , op : *const c_char) -> c_double {
-    let operation = unsafe {
-        CStr::from_ptr(op)
-    }.to_string_lossy();
+fn compute(first: c_double, second: c_double, op: *const c_char) -> c_double {
+    let operation = unsafe { CStr::from_ptr(op) }.to_string_lossy();
 
     match operation.as_ref() {
         "add" => first + second,
         "sub" => first - second,
         "mul" => first * second,
         "div" => first / second,
-        _ => 0.0
+        _ => 0.0,
     }
 }
 
 #[no_mangle]
-fn transform(data : *mut c_double , len : usize) {
-    let values = unsafe {std::slice::from_raw_parts_mut(data, len)};
+fn transform(data: *mut c_double, len: usize) {
+    let values = unsafe { std::slice::from_raw_parts_mut(data, len) };
     values.reverse();
-    values.iter_mut().for_each(|x|{
+    values.iter_mut().for_each(|x| {
         *x += 10.0;
     });
-
 }
---
+++ mini_python_rust/target/debug/deps/mini_python_rust-e9d7507d27ae09b1.d
@@ -0,0 +1,9 @@
+C:\Users\sylan\Documents\Rust\S3_PRG_Rust\mini_python_rust\target\debug\deps\libmini_python_rust-e9d7507d27ae09b1.rmeta: src\lib.rs Cargo.toml
+
+C:\Users\sylan\Documents\Rust\S3_PRG_Rust\mini_python_rust\target\debug\deps\mini_python_rust-e9d7507d27ae09b1.d: src\lib.rs Cargo.toml
+
+src\lib.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
